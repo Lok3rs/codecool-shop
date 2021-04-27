b2c3a8962031d5a679965d19ef47086611de59cd
Dawid Adamski <lok3rsss@gmail.com>
2021-04-27 18:08:48 +0200

    Create CartController and pin it to new template

---
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -0,0 +1,25 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns = {"/cart"})
+public class CartController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        engine.process("cart/cart.html", context, resp.getWriter());
+    }
+}
