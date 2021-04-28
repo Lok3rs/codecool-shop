650c4199e6fff5edd308a397de9c80543041746c
Dawid Adamski <lok3rsss@gmail.com>
2021-04-28 23:48:33 +0200

    Implement add products to cart functionality

--- src/main/java/com/codecool/shop/controller/AddToCartController.java
+++ src/main/java/com/codecool/shop/controller/AddToCartController.java
@@ -1,23 +1,35 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductDao;
+
+import com.codecool.shop.dao.implementation.CartDaoMem;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.model.Product;
+import com.codecool.shop.service.CartService;
 import com.codecool.shop.service.ProductService;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+@WebServlet(urlPatterns = {"/add_to_cart"})
 public class AddToCartController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        ProductService productService = new ProductService(ProductDaoMem.getInstance(), ProductCategoryDaoMem.getInstance(), SupplierDaoMem.getInstance());
+        CartService cartService = new CartService(CartDaoMem.getInstance());
+        List<Product> products = productService.getAllProducts();
+        for (Product product : products) {
+            if(req.getParameter(product.getName()) != null){
+                cartService.addToCart(product);
+            }
+        }
+        resp.sendRedirect("/cart");
     }
 }
--- src/main/webapp/static/js/cart.js
+++
@@ -1,8 +0,0 @@
-let addToCartButtons = document.querySelectorAll(".addToCart")
-
-addToCartButtons.forEach(btn => {
-    btn.addEventListener("click", () => {
-        let cartService = Java.type('com.codecool.shop.service.CartService')
-        console.log(btn.value)
-    })
-})
